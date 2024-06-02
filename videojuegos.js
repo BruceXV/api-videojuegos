e4968edc056e658c1e99265da4ce10156d430c42
bruce <bruceantonio142004@gmail.com>
2024-06-02 18:25:18 -0400

    actualizacion del api

--- videojuegos.js
+++ videojuegos.js
@@ -1,27 +1,84 @@
-
 //esto es la api
 
 const videojuegos = [
     {
         "id":1,
-        "nombre":"universidad de chile",
-        "pais":"chile"
+        "nombre":"Shadow of war",
+        "Género":"Acción",
+        "año de lanzamiento":"21 de noviembre de 2014",
+       "Clasificación": "M",
+       "imagen": "shadowOfwar.jpg"  // Añade la ruta de la imagen aquí
+       
     },
     {
         "id":2,
-        "nombre":"san lorenzo",
-        "pais":"argentina"
+        "nombre":"The Escapists",
+        "Género":"Acción",
+        "año de lanzamiento":"20 de agosto de 2014",
+       "Clasificación": "M",
+       "imagen": "ruta/a/la/imagen1.jpg"
     },
     {
         "id":3,
-        "nombre":"vasco de gama",
-        "pais":"brasil"
+        "nombre":"Lethal Company",
+        "Género":" aventura de acción cooperativa y supervivencia",
+        "año de lanzamiento":"octubre de 2023 ",
+       "Clasificación": "M",
+       "imagen": "ruta/a/la/imagen1.jpg"
     }
     ,
     {
         "id":4,
-        "nombre":"cienciano",
-        "pais":"peru"
+        "nombre":"Undertale",
+        "Género":" Juego de rol",
+        "año de lanzamiento":"15 de septiembre de 2015",
+       "Clasificación": "M",
+       "imagen": "ruta/a/la/imagen1.jpg"
+    }
+    ,
+    {
+        "id":5,
+        "nombre":"Dragon ball Xenoverse",
+        "Género":" Juego de rol",
+        "año de lanzamiento":"15 de septiembre de 2015",
+       "Clasificación": "M",
+       "imagen": "ruta/a/la/imagen1.jpg"
+    }
+    ,
+    {
+        "id":6,
+        "nombre":"Content Warning",
+        "Género":"cooperativo de terror y supervivencia ",
+        "año de lanzamiento":"1 de abril de 2024",
+       "Clasificación": "M",
+       "imagen": "ruta/a/la/imagen1.jpg"
+    }
+    ,
+    {
+        "id":7,
+        "nombre":"Left 4 Dead 2/*",
+        "Género":"disparos en primera persona cooperativo de tipo videojuego de terror ",
+        "año de lanzamiento":"17 de noviembre de 2009",
+       "Clasificación": "M",
+       "imagen": "ruta/a/la/imagen1.jpg"
+    }
+    ,
+    {
+        "id":8,
+        "nombre":"Call Of Duty Warzone",
+        "Género":" disparos en primera persona5",
+        "año de lanzamiento":"10 de marzo de 2020",
+       "Clasificación": "M",
+       "imagen": "ruta/a/la/imagen1.jpg"
+    }
+    ,
+    {
+        "id":9,
+        "nombre":"Star Wars Battlefront Classic Collection",
+        "Género":" JAcción, disparos en primera persona, disparos en tercera persona",
+        "año de lanzamiento":"14 de marzo de 2024",
+       "Clasificación": "M",
+       "imagen": "ruta/a/la/imagen1.jpg"
     }
 ];
 
